607233b7b59eb1cc7add63f399af57c478808db5
Hamzeh Naser <97958695+hmznaser@users.noreply.github.com>
2022-07-16 11:14:59 +0300

    Add files via upload

---
+++ blog/js/script.js
@@ -0,0 +1,8 @@
+
+    const toggleButton = document.getElementById('toggle-button');
+	const navilist = document.getElementById('navi-list');
+
+	toggleButton.addEventListener('click', () => {
+	   navilist.classList.toggle('active');	
+	})
+ 
